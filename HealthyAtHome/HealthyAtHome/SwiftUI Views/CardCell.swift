dd5ccdd1940729592b4ba8c900bc9e92302cc66c
Youmin <772069508@qq.com>
2021-07-21 17:32:36 -0400

    Add comments

--- HealthyAtHome/HealthyAtHome/SwiftUI Views/CardCell.swift
+++ HealthyAtHome/HealthyAtHome/SwiftUI Views/CardCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// Protocol to tell specific View Controller what function to use.
 protocol ProfileCellDelegate: AnyObject {
     func didTapProfileButton(with title: String)
 }
@@ -14,16 +15,29 @@ protocol ProfileCellDelegate: AnyObject {
 protocol SleepTrackerCellDelegate: AnyObject {
     func didTapSleepTrackerButton(with title: String)
 }
-
+// Todo:
+        // Add protocol for (if necessary)
+                // 1. Home Page
+                // 2. Food diary
+                // 3. Workout
 
 class CardCell: UITableViewCell {
     
+    // Defult setting
     private var defultTitle: String = ""
     static let identifier = "CardCell"
+    
+    // Delegate for each page
     weak var delegateProfile: ProfileCellDelegate?
     weak var delegateSleepTracker: SleepTrackerCellDelegate?
+    // Todo:
+            // Add delegate for (if necessary)
+                // 1. Home Page
+                // 2. Food diary
+                // 3. Workout
     
     
+    // Button on each card (I made the button invisiable, but you can check the storyboard and see it)
     @IBOutlet weak var profileButton: UIButton!
     @IBAction func didTapProfileButton() {
         delegateProfile?.didTapProfileButton(with: defultTitle)
@@ -32,6 +46,12 @@ class CardCell: UITableViewCell {
     @IBAction func didTapSleepTrackerButton() {
         delegateSleepTracker?.didTapSleepTrackerButton(with: defultTitle)
     }
+    // Todo:
+            // Add buttons for (if necessary)
+                // 1. Home Page
+                // 2. Food diary
+                // 3. Workout
+    
     
     // Sleep Tracker
     @IBOutlet weak var cardView_ST: UIView!
@@ -51,15 +71,20 @@ class CardCell: UITableViewCell {
     @IBOutlet weak var unit3_Pro: UILabel!
     @IBOutlet weak var unit4_Pro: UILabel!
     
+    // Todo:
+            // implement elements for
+                // 1. Home Page
+                // 2. Food diary
+                // 3. Workout
     
-    // Set up the cell for sleep tracker
+    // Set up the cell for Sleep tracker
     func configureSleepTracker(title: String, unit: String, hour: String) {
         self.defultTitle = title
         titleLabel_ST.text = title
         unitLabel_ST.text = unit
         hourLabel_ST.text = hour
     }
-    
+    // Set up the cell for Profile
     func configureProfile(title: String, data1: String, data2: String, data3: String, data4: String,
                           unit1: String, unit2: String, unit3: String, unit4: String) {
         self.defultTitle = title
@@ -73,4 +98,10 @@ class CardCell: UITableViewCell {
         unit3_Pro.text = unit3
         unit4_Pro.text = unit4
     }
+    
+    // Todo:
+        // Add configuration for
+            // 1. Home Page
+            // 2. Food diary
+            // 3. Workout
 }
--- HealthyAtHome/HealthyAtHome/ViewController.swift
+++ HealthyAtHome/HealthyAtHome/ViewController.swift
@@ -17,48 +17,60 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
+// Todo:
+    // Add View Controller for (if necessary)
+        // 1. Home Page
+        // 2. Food diary
+        // 3. Workout
 
 
-// Sleep Tracker ----------------------------------------------------------------------
+// Sleep Tracker -----------------------------------------------------------------------------------------------------------------------------
 class SleepTrackerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // Link to Card View
     @IBOutlet weak var CardTableView: UITableView!
     
+    // Some Testing Data
     let titles_ST: [String] = ["Sleep Data", "Sleep Goal", "Weekly Average", "Wake Up Goal", "Recommended Bedtime"]
     let unit: [String] = ["Hours","Hours","Hours","am","pm"]
     let hour: [String] = ["0","0","0","0","0"]
     
-    
+    // Running the View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemIndigo
         
+        // Register to recognize which View is using, in this case using Sleep Tracker page
         CardTableView.register(CardCell.self, forCellReuseIdentifier: CardCell.identifier)
         CardTableView.dataSource = self
     }
     
-    // How many rows in the tableview
+    // How many rows in the card view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return titles_ST.count
     }
-
+    
+    // Add defult data into the cell, and change cell format
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! CardCell
         cell.cardView_ST.layer.borderWidth = 1
         cell.cardView_ST.layer.borderColor = UIColor.black.cgColor
         cell.cardView_ST.layer.cornerRadius = 30
         
-        cell.configureSleepTracker(title: titles_ST[indexPath.row], unit: unit[indexPath.row], hour: hour[indexPath.row])
         cell.delegateSleepTracker = self
+        cell.configureSleepTracker(title: titles_ST[indexPath.row], unit: unit[indexPath.row], hour: hour[indexPath.row])
+        
         return cell
     }
 }
 
-
+// Profile -----------------------------------------------------------------------------------------------------------------------------
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // Link to Card View
     @IBOutlet weak var CardTableView: UITableView!
     
+    // Some Testing Data
     let titles_Pro: [String] = ["User Info", "Workouts", "Food Diet", "Sleep Tracker"]
     let data1: [String] = ["data1","data1","data1","data1"]
     let data2: [String] = ["data2","data2","data2","data2"]
@@ -69,12 +81,12 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     let unit3: [String] = ["unit3","unit3","unit3","unit3"]
     let unit4: [String] = ["unit4","unit4","unit4","unit4"]
 
-    
-    
+    // Running the View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         
+        // Register to recognize which View is using, in this case using Profile page
         CardTableView.register(CardCell.self, forCellReuseIdentifier: CardCell.identifier)
         CardTableView.dataSource = self
         
@@ -85,6 +97,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         return titles_Pro.count
     }
 
+    // Add defult data into the cell, and change cell format
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! CardCell
         cell.cardView_Pro.layer.borderWidth = 1
@@ -99,14 +112,18 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     
 }
 
-extension ProfileViewController: ProfileCellDelegate {
-    func didTapProfileButton(with title: String) {
-        print (title)
-    }
-}
+// Control each individual cell in Sleep Tracker page
 extension SleepTrackerViewController: SleepTrackerCellDelegate {
     func didTapSleepTrackerButton(with title: String) {
         print (title)
     }
     
 }
+
+// Control each individual cell in Profile page
+extension ProfileViewController: ProfileCellDelegate {
+    func didTapProfileButton(with title: String) {
+        print (title)
+    }
+}
+

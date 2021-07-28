3211b586c22914e4372a0612a44fdb4865ab7e1c
Youmin <772069508@qq.com>
2021-07-27 21:29:35 -0400

    Easier plan
    Editing functions are competed

---
+++ HealthyAtHome/HealthyAtHome/SwiftUI Views/SleepTrackerChartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SleepTrackerChartViewController.swift
+//  HealthyAtHome
+//
+//  Created by Youmin Zhou on 7/27/21.
+//
+
+import UIKit
+
+class SleepTrackerChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemIndigo
+    }
+    
+}

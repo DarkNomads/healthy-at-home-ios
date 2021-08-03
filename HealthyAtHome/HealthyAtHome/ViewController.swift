ab0529fb40043d98375aa818ab2760513a99c357
David Cao <davidcao1337@gmail.com>
2021-08-03 14:22:07 -0400

    Revert "Merge branch 'David_branch' into main"
    This reverts commit 14a57dd92334e36c67069001b83fd9b20f3b2a0d, reversing
    changes made to 43593a6074962fabd46b7e3f9a84d34dd5dee111.

--- HealthyAtHome/HealthyAtHome/ViewController.swift
+++ HealthyAtHome/HealthyAtHome/ViewController.swift
@@ -48,6 +48,29 @@ class ViewController: UIViewController {
     }
 }
 
+// Home-----------------------------------------------------------------------------------------------------------------------------
+
+class HomePageViewController: UIViewController {
+    
+    @IBOutlet weak var calorieRemainLb: UILabel!
+    @IBOutlet weak var averageSleepLb: UILabel!
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    // Running the View Controller
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshButton.layer.cornerRadius = 10
+    }
+    
+    @IBAction func didTapRefreshButton(_ sender: Any) {
+        calorieRemainLb.text = globalCalRemain
+        averageSleepLb.text = String(format: "%.2f", globalSleepData.reduce(0, +) / Double(globalSleepData.count))
+    }
+}
+
+
+
 // Food Diary -----------------------------------------------------------------------------------------------------------------------------
 
 class FoodDiaryViewController: UIViewController{
@@ -157,12 +180,43 @@ class SleepTrackerViewController: UIViewController {
     }
     
     @IBAction func didTapChartButton(_ sender: Any) {
-        guard let chartViewCtl = storyboard?.instantiateViewController(identifier: "chart") as? SleepTrackerChartViewController else {
-            print ("Failed to get chart ViewController from storyboard")
+        guard let settingViewCtl = storyboard?.instantiateViewController(identifier: "sleepSetting") as? SleepTrackerSettingViewController else {
+            print("Failed to get chart ViewController from storyboard")
             return
         }
-        // chart goes here
-        present(chartViewCtl, animated: true)
+        // Set wake up and bed time
+        settingViewCtl.hourHandlder = { text in
+            self.sleepHourLb.text = text
+        }
+        settingViewCtl.minuteHandler = { text in
+            self.sleepMinuteLb.text = text
+        }
+        settingViewCtl.wakeUpHandler = { text in
+            self.wakeUpTimeLb.text = text
+        }
+        settingViewCtl.bedTimeHandler = { text in
+            self.bedTimeLb.text = text
+        }
+        settingViewCtl.averageHourHandler = { text in
+            self.averageHourLb.text = text
+        }
+        settingViewCtl.averageMinuteHandler = { text in
+            self.averageMinuteLb.text = text
+        }
+        present(settingViewCtl, animated: true)
+        
+        // debug section
+        print ("Sleep data array: ", globalSleepData)
+        print ("Average hour: ", globalWeeklyAverageHours)
+        print ("Average minute: ", globalWeeklyAverageMinutes)
+        print ("Wake up: ", globalWakeUpTime!)
+        print ("Bed Time: ", globalBedTime!)
+//        guard let chartViewCtl = storyboard?.instantiateViewController(identifier: "chart") as? SleepTrackerChartViewController else {
+//            print ("Failed to get chart ViewController from storyboard")
+//            return
+//        }
+//        // chart goes here
+//        present(chartViewCtl, animated: true)
     }
     
     // Button Weekly Average
